376e379a2f667fd1cb6dee577fbaa2385ad1baca
lwloveyou <qianfeng@qianfengdeMacBook-Pro-39.local>
2015-03-27 21:32:21 +0800

    test
    shi

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by qianfeng on 15/3/27.
+//  Copyright (c) 2015年 刘伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
